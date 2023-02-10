fcbfbe9b5dc42d4e9eea74938c082bda639921e8
KatyaBahai <katyxa-two@yandex.ru>
2023-02-10 23:06:13 +0600

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,26 @@
+/*package ru.job4j.set;
+
+import ru.job4j.collection.SimpleArrayList;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class SimpleSet<T> implements Set<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(0);
+
+    @Override
+    public boolean add(T value) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+}*/

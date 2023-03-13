c4503536dbc52b4dc84a2e242e45d64e78cf7b39
KatyaBahai <katyxa-two@yandex.ru>
2023-03-13 17:36:44 +0600

    8. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -9,15 +9,16 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class AnalysisTest {
 
     @Test
-    void if2PeriodsUnavailable (@TempDir Path tempDir) throws IOException {
+    void if2PeriodsUnavailable(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("source.txt").toFile();
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01\n" +
-                    "300 10:57:01\n" +
-                    "400 10:58:01\n" +
-                    "300 10:59:01\n" +
-                    "500 11:01:02\n" +
-                    "200 11:02:02");
+            out.println("""
+                    200 10:56:01
+                    300 10:57:01
+                    400 10:58:01
+                    300 10:59:01
+                    500 11:01:02
+                    200 11:02:02""");
         }
         File target = tempDir.resolve("target.txt").toFile();
 

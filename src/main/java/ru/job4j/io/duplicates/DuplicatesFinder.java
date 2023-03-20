6f40e1d338deeefe0ae9e978d0d73c211971cb09
KatyaBahai <katyxa-two@yandex.ru>
2023-03-20 15:33:39 +0600

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("C:\\Users\\Honor\\Pictures\\Camera Roll"), new DuplicatesVisitor());
+        DuplicatesVisitor visitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("C:\\Users\\Honor\\Pictures\\Camera Roll"), visitor);
+        visitor.printDuplicates();
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,9 +1,7 @@
 package ru.job4j.io.duplicates;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -18,28 +16,25 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-
         if (attrs.isRegularFile()) {
             FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
-            Files.walkFileTree(file, new SimpleFileVisitor<>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-                    if (map.containsKey(fileProperty)) {
-                        map.get(fileProperty).add(file.toAbsolutePath().toString());
-                    } else {
-                        ArrayList<String> list = new ArrayList<>();
-                        list.add(file.toAbsolutePath().toString());
-                        map.put(fileProperty, list);
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-
-            if (map.get(fileProperty).size() > 1) {
-                System.out.printf("%s - %d%n", fileProperty.getName(), fileProperty.getSize());
-                map.get(fileProperty).forEach(System.out::println);
+            if (map.containsKey(fileProperty)) {
+                map.get(fileProperty).add(file.toAbsolutePath().toString());
+            } else {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(file.toAbsolutePath().toString());
+                map.put(fileProperty, list);
             }
         }
         return FileVisitResult.CONTINUE;
+    }
+
+    public void printDuplicates() {
+        for (Map.Entry<FileProperty, List<String>> entry : map.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                System.out.printf("%s - %d%n", entry.getKey().getName(), entry.getKey().getSize());
+                entry.getValue().forEach(System.out::println);
+            }
+        }
     }
 }

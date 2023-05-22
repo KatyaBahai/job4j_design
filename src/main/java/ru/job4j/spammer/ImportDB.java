dbee6a62091a9321ba2b54ba4d9fde13bcf0a144
KatyaBahai <katyxa-two@yandex.ru>
2023-05-22 15:29:00 +0600

    0.2. PreparedStatement [#379307]

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -21,6 +21,9 @@ public class ImportDB {
             users = rd.lines()
                     .map(p -> {
                         String[] split = p.split(";");
+                       if (split.length != 2) {
+                           throw new IllegalArgumentException();
+                        }
                         return new User(split[0], split[1]);
                     })
                     .collect(Collectors.toList());
@@ -72,6 +75,14 @@ public class ImportDB {
                 + "%s text);", tableName, col1, col2);
         try (Statement statement = cnt.createStatement()) {
             statement.executeUpdate(create);
+            ResultSet selection = statement.executeQuery(String.format(
+                    "SELECT * FROM %s LIMIT 1", tableName
+            ));
+            ResultSetMetaData metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                System.out.printf("%-15s|%-15s%n%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
